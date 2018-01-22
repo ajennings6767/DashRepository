5373cf237390badf7a1d7671fc45d52cda88b870
ajennings6767 <30580411+ajennings6767@users.noreply.github.com>
2018-01-22 09:52:25 -0500

    a

--- appUtils.py
+++ appUtils.py
@@ -1,8 +1,6 @@
 import Utils
 
 cm = Utils.CIXSManager()
-cm.does_CIXS_Master_Exist()
-cm.create_CIXS_Tables()
+
+print("violsidnm")
 cm.CIXS_Update_Table_Data()
-cm.CIXS_Master_Add_Field("zScore_30", "YES")
-cm.CIXS_Table_Refresh_Fields()
